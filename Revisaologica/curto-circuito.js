0ff859f9e504ba4f32f46824b7b026479bcf8003
Gustavo Pucienik <gomespucienik@gmail.com>
2022-11-20 18:25:14 -0300

    primeiro commit

---
+++ Revisaologica/curto-circuito.js
@@ -0,0 +1,14 @@
+let n = 0;
+
+n = n || 10;
+
+console.log(n);
+
+let isValid = false;
+
+//if(isValid){
+//    console.log("é valido")
+//};
+
+//isValid && console.log("é valido") <-
+isValid || console.log("é valido")
