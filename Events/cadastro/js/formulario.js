7ad83fb68ff546f718126f1516477a8ff6c63d15
Gustavo Pucienik <gomespucienik@gmail.com>
2023-01-05 17:10:47 -0300

    cancelando envio do formulario

--- Events/cadastro/js/formulario.js
+++ Events/cadastro/js/formulario.js
@@ -1,3 +1,25 @@
 (function(){
     'use strict';
+
+    const txtTitulo = document.getElementById("txtTitulo")
+    const btn = document.getElementById("btn")
+    const formCadastro = document.querySelector(".formCadastro")
+
+    /* btn.addEventListener("click", function (e){
+        console.log(txtTitulo.value)
+        if(!txtTitulo.value){
+            alert("preencha todos os campos")
+            e.preventDefault()
+            txtTitulo.focus()
+        }
+    }) */
+
+    formCadastro.addEventListener("submit", function (e){
+        console.log(txtTitulo.value)
+        if(!txtTitulo.value){
+            alert("preencha todos os campos")
+            e.preventDefault()
+            txtTitulo.focus()
+        }
+    })
 })()
