27586d11dd1e2a4eac637bc5efad020a90e4a7f8
Gustavo Pucienik <gomespucienik@gmail.com>
2023-01-08 19:17:01 -0300

    adicionando li

---
+++ Events/todolist/js/todolist.js
@@ -0,0 +1,20 @@
+;(function(){
+    'use strict'
+
+    //Armazenar o dom em variaveis
+
+    const itemInput = document.getElementById("item-input")
+    const todoAddForm = document.getElementById("todo-add")
+    const ul = document.getElementById("todo-list")
+
+    todoAddForm.addEventListener("submit", function(e){
+        e.preventDefault()
+
+        ul.innerHTML += `
+        <li class="todo-item">
+                    <p class="task-name">${itemInput.value}</p>
+
+                </li>`
+    })
+
+})()
