27e0c5b37593a05d1b2faebf2da5109144c1b97c
Gustavo Pucienik <gomespucienik@gmail.com>
2023-01-15 17:09:39 -0300

    estudando stings

--- Strings/introducao.js
+++ Strings/introducao.js
@@ -10,7 +10,7 @@ startsWith(),    endsWith(),
 charAt,          charCode(),       e  length
 */
 
-let str1 = "minha string bacanuda"
+let str1 = "Minha string bacanuda"
 
 //console.log(str1.replace("i","e"))
 //console.log(str1.replace(/i/g, "e"))
@@ -21,11 +21,40 @@ let str1 = "minha string bacanuda"
 //console.log(str1.lastIndexOf("i"))
 //console.log(str1.includes("i")) //se tem retorna boolean
 
-//console.log(str1.slice(0, 6))
-//console.log(str1.substring(6,12))
-
+//console.log(str1.slice(0, 6)) não da pra colocar do maior pro menor
 //console.log(str1.slice(-15))//nao da pra colocar 0, coloca só 1 numero
 //console.log(str1.slice(-15, -9))
-
-//console.log(str1.slice(12, 6)) não da pra colocar do maior pro menor
+//console.log(str1.substring(6,12))
 //console.log(str1.substring(16, 6))// substring da pra colocar do maior para o menor
+
+//console.log(str1.toUpperCase())
+//console.log(str1.toLowerCase())
+
+let strAsObj = new String("Minha nova string como objeto")
+
+console.log(strAsObj)
+console.log(strAsObj.valueOf())//toString()funciona igual para strings
+
+str1 = "    " + str1 + "      "
+/* console.log(str1)
+console.log(str1.trim())
+console.log(str1.trimEnd())
+console.log(str1.trimStart()) */
+
+/* str1 = "0123456789"
+console.log(str1.padStart(20))
+console.log(str1.padStart(20, "-"))
+console.log(str1.padEnd(20, "-"))
+console.log(str1) */
+
+let telefone = "99860-3050"
+let telefone0 = "9860-3050"
+
+function mascara(num){
+    let hifenPosi = num.indexOf("-")
+    let numInicio = num.slice(0, hifenPosi)
+    let numFim = num.slice(hifenPosi + 1)
+    return `${numInicio[0].padEnd(numInicio.length, "*")} `
+}
+mascara(telefone)
+console.log(telefone)
