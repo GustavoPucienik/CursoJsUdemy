3c7a0c80892121a1c819e73e3094518c03061600
Gustavo Pucienik <gomespucienik@gmail.com>
2023-01-18 19:50:26 -0300

    Desafio Math

--- Math/sorteio.js
+++ Math/sorteio.js
@@ -1,8 +1,6 @@
-function getRamdomNumber(inicio = "", fim, integer){
+function getRamdomNumber(inicio, fim, integer){
     
     let r = Math.random() * (fim - inicio + 1) + inicio
     return integer == true ? parseInt(r) : r
 
 }
-
-console.log(getRamdomNumber(5, 10, true))
