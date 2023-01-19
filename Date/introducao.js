7cc7a7d254d58d0275e930fbc9c934bbde947870
Gustavo Pucienik <gomespucienik@gmail.com>
2023-01-19 18:04:48 -0300

    revendo os principais metodos Date

---
+++ Date/introducao.js
@@ -0,0 +1,13 @@
+const data = new Date()
+console.log(data)           //RETORNA DATA
+console.log(data.getDay())  // DIA DA SEMANA COMEÇA EM 0 NO DOMINGO
+console.log(data.getDate()) //DIA(COMEÇA DO 1)
+console.log(data.getMonth())// MES(COMEÇA POR 0)
+console.log(data.getTime()) // DATA EM TIMESTAMP
+console.log(data.getFullYear())//ANO
+console.log(data.getMilliseconds()) //milisegundos
+console.log(data.getHours())    //Horario
+console.log(data.getUTCHours()) // horario mundial
+
+///UTC universal
+///set para passar novo valor
