c69d8511ae3e5d6afb3b1919382cd08f3d40a68b
Gustavo Pucienik <gomespucienik@gmail.com>
2023-01-20 19:34:25 -0300

    Resolução do exercicio.

--- Date/date_birthday/js/birthday.js
+++ Date/date_birthday/js/birthday.js
@@ -1,4 +1,24 @@
 function quantoFaltaPara(m, d) {
+    const dataNow = new Date()
+    dataNow.setHours(0)
+    dataNow.setMinutes(0)
+    dataNow.setSeconds(0)
+    dataNow.setMilliseconds(0)
 
+    let yearNow = dataNow.getFullYear()
+
+    let dataBirthday = new Date(yearNow, m-1, d)
+
+    const dataNowTS = +dataNow
+    const dataBirthdayTS = dataBirthday
+
+    if(dataBirthdayTS < dataNowTS){
+        dataBirthday.setFullYear(++yearNow)
+        dataBirthdayTS = +dataBirthday
+    }
+
+    const oneDay = 24 * 60 * 60 * 1000
+    const difference = dataBirthdayTS - dataNowTS
+    return parseInt(difference / oneDay)
 
 }
