d513aaa6b0f45dd6eed565b76ab24a7859d86a12
Gustavo Pucienik <gomespucienik@gmail.com>
2023-01-21 19:51:10 -0300

    Relembrando BOM

---
+++ Bom/introducao.js
@@ -0,0 +1,18 @@
+console.log(innerHeight)//propriedade que retorna tamanho da area util do browser
+console.log(innerWidth) //se o zoom pode alterar o valor dessas variaveis
+console.log(outerHeight)//propriedade que retorna tamanho do browser
+console.log(outerWidth)
+
+console.log(screenX)//posição do navegador na tela
+console.log(screenY)
+console.log(screen)//.availWidth, width 
+
+
+console.log(history)//history.back() history.forward()
+
+console.log(location)// .href, .hash, .search, .protocol, .pathname
+        //.assign navega e altera o history.length, .reload apenas recarrega a pagina
+        //.replace navega e não altera o history.length,
+
+console.log(navigator)//.userAgent, .plugins, .cookies, .cookiesEnabled
+    //.plataform mostra o sistema operacional que carrega a pagina
