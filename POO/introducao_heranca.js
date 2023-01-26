6f952875623e61a68c4428e9617eae6d502796a1
Gustavo Pucienik <gomespucienik@gmail.com>
2023-01-26 20:48:32 -0300

    hasOwnPrototype, instanceOf, isPrototypeOf, getPrototypeOf

--- POO/introducao_heranca.js
+++ POO/introducao_heranca.js
@@ -12,8 +12,28 @@ Animal.prototype.tipo = "desconhecido"
 function Cachorro (nome, tipo){
     this.nome = nome
     Animal.call(this, tipo)
-    this.constructor = Cachorro
+    //this.constructor = Cachorro
+    Object.defineProperty(Cachorro.prototype, "constructor",{
+        value: Cachorro,
+        enumerable: false
+    })
 }
 Cachorro.prototype = new Animal()
 
 let rex = new Cachorro("Rex", "Mamifero")
+
+for(let prop in rex){
+    if(rex.hasOwnProperty(prop)){
+        console.log(prop)
+    }
+    
+}
+
+console.log(rex instanceof Cachorro)//true
+console.log(rex instanceof Object)//true
+console.log(rex instanceof Array)//false
+console.log(Cachorro.prototype.isPrototypeOf(rex))//true
+console.log(Animal.prototype.isPrototypeOf(rex))//true
+console.log(Object.prototype.isPrototypeOf(rex))//true
+console.log(Object.getPrototypeOf(rex))//Animal{}
+console.log(rex.__proto__)//
