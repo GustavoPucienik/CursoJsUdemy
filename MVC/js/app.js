4bdaeb574e70cbbe2ad61782fb9a7449c495c60a
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-04-09 19:16:51 -0300

    commit de teste

--- MVC/js/app.js
+++ MVC/js/app.js
@@ -5,4 +5,14 @@ alunos.forEach(aluno => {
         aluno.media[materia] = avarege(...aluno.notas[materia])
     }
 })
-console.log(alunos)
+
+//inserir no thead "nome" e cada uma das materias
+const htmlHeader = document.createElement("tr")
+htmlHeader.innerHTML = "<td>Nome</td>"
+
+let htmlMaterias = ""
+const htmlheaderMaterias = Object.keys(alunos[0].notas).map(materia => {
+    console.log(materia)
+    return "<td>" + materia + "</td>"
+})
+console.log(htmlheaderMaterias)
