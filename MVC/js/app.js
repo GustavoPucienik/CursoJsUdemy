0d060ec6edc0526e162258aec92144d5dad45167
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-04-11 17:17:10 -0300

    adicionar aluno

--- MVC/js/app.js
+++ MVC/js/app.js
@@ -20,7 +20,9 @@ htmlHeader.innerHTML += htmlheaderMaterias
 document.querySelector("[data-table-alunos] thead").appendChild(htmlHeader)
 
 // percorrer cada aluno e gerar o html para incluir do tbody
-alunos.forEach( aluno => {
+function render(){
+    document.querySelector("[data-table-alunos] tbody").innerHTML = ""
+    alunos.forEach( aluno => {
     const htmlBody = document.createElement("tr")
     let htmlMedias = `<td>${aluno.nome}</td>`
     Object.keys(aluno.notas).forEach( materia => {
@@ -28,4 +30,34 @@ alunos.forEach( aluno => {
     })
     htmlBody.innerHTML = htmlMedias
     document.querySelector("[data-table-alunos] tbody").appendChild(htmlBody)
+})
+}
+
+render()
+
+
+//adicionar alunos
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const nome = document.getElementById("first_name").value
+    const newAluno = {
+        _id: 0,
+        nome,
+        notas: {
+          portugues: [10, 10, 2, 2],
+          matematica: [2, 10, 2, 2],
+          historia: [10, 7, 3, 5],
+          ciencias: [9, 8, 5, 3],
+        },
+      }
+
+      newAluno.media = {}
+
+      for(let materia in newAluno.notas){
+        newAluno.media[materia] = avarege(...newAluno.notas[materia])
+    }
+
+      alunos.push(newAluno)
+
+      render()
 })
